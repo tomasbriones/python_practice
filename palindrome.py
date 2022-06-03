6267891f92d7f133b80f4736432870e79c14d1fe
Tomas Briones <tomasbriones@gmail.com>
2022-06-02 22:26:13 -0400

    input

--- palindrome.py
+++ palindrome.py
@@ -5,7 +5,8 @@ def is_palindrome(string: str)-> bool:
     return string == string[::-1]
 
 def run():
-    print(is_palindrome(1000))
+    var = input("Ingrese un nombre:")
+    print(is_palindrome(var))
 
 if __name__ == '__main__':
     print("Ejecutando codigo")
